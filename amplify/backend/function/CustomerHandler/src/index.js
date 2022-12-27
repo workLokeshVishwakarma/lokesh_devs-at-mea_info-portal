66fd217bcfa87d804043ed4f96395bd22e8e2e20
meaLokeshVishwakarma <work.lokeshvishwakarma@gmail.com>
2022-12-27 10:18:32 +0530

    add-amplify-hoisting-and-update-appJSCode

--- amplify/backend/function/CustomerHandler/src/index.js
+++ amplify/backend/function/CustomerHandler/src/index.js
@@ -5,13 +5,15 @@
  */
 exports.handler = async (event) => {
     console.log(`EVENT: ${JSON.stringify(event)}`);
+    const customerID = event.pathParameters.customerID;
+    const customer = {"customerID": customerID, customerName: `Customer ${customerID}`};
     return {
         statusCode: 200,
     //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
+     headers: {
+         "Access-Control-Allow-Origin": "*",
+         "Access-Control-Allow-Headers": "*"
+     }, 
+        body: JSON.stringify(customer),
     };
 };
--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,17 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "CustomerHandler": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "apic1bc53ec": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
