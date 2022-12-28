ba71e6d8f3a86c5764fb074f6e4953674734f5d5
meaLokeshVishwakarma <work.lokeshvishwakarma@gmail.com>
2022-12-28 11:12:50 +0530

    hotfix/implamented-basic-login-signup-username-based-email-mail-otp-vefify-auth-with-core-aws-amplify-with-Authenticator

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "lokeshmeadevsinfoapp1678a7b7": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import { Amplify } from "aws-amplify";
+import "@aws-amplify/ui-react/styles.css";
+import { Authenticator } from "@aws-amplify/ui-react";
+
+import awsExports from "./aws-exports";
+
+Amplify.configure(awsExports);
+
 const App = () => {
   return (
     <>
-      <div className="App">
-        <h1>App</h1>
-      </div>
+      <Authenticator>
+        {({ signOut, user }) => (
+          <div className="App">
+            <p>Welcome, {user.username}!</p>
+            <button style={{cursor: "pointer"}} onClick={signOut}>Sign out</button>
+          </div>
+        )}
+      </Authenticator>
     </>
   );
 };
